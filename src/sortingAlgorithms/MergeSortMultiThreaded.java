5a63bdedf64005242de8a8182fff9298673e12ae
Sibendu Dey <sibendu.dey@gmail.com>
2017-06-18 00:09:57 +0530

    MergeSortMultiThreadedProgram

--- src/sortingAlgorithms/MergeSortMultiThreaded.java
+++ src/sortingAlgorithms/MergeSortMultiThreaded.java
@@ -9,18 +9,20 @@ import java.util.Scanner;
 
 public class MergeSortMultiThreaded {
 
-    public static void main(String args[])   {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int arr[] = new int[N];
-        for ( int i = 0 ; i < N ; i++)
+        for (int i = 0; i < N; i++)
             arr[i] = sc.nextInt();
 
-        Thread t = new Thread(() -> {mergeSort(arr, 0 , N - 1);});
+        Thread t = new Thread(() -> {
+            mergeSort(arr, 0, N - 1);
+        });
         t.start();
         try {
             t.join();
-            Arrays.stream(arr).forEach( (i) -> System.out.print(i + " "));
+            Arrays.stream(arr).forEach((i) -> System.out.print(i + " "));
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -29,10 +31,14 @@ public class MergeSortMultiThreaded {
 
     private static void mergeSort(int[] arr, int i, int j) {
 
-        if ( i < j ) {
+        if (i < j) {
             int k = (i + j) / 2;
-            Thread t1 = new Thread(() -> {mergeSort(arr , i , k);});
-            Thread t2 = new Thread(() -> {mergeSort(arr, k + 1, j);});
+            Thread t1 = new Thread(() -> {
+                mergeSort(arr, i, k);
+            });
+            Thread t2 = new Thread(() -> {
+                mergeSort(arr, k + 1, j);
+            });
             t1.start();
             t2.start();
             try {
@@ -52,33 +58,32 @@ public class MergeSortMultiThreaded {
         int arr1[] = new int[N1];
         int arr2[] = new int[N2];
 
-        for ( int i1 = 0 ; i1 < N1; i1++)    {
+        for (int i1 = 0; i1 < N1; i1++) {
             arr1[i1] = arr[i + i1];
         }
 
-        for ( int i2 = 0 ; i2 < N2; i2++)   {
+        for (int i2 = 0; i2 < N2; i2++) {
             arr2[i2] = arr[i2 + k + 1];
         }
 
         int i1 = 0, i2 = 0;
-        while ( i1 < N1 && i2 < N2)   {
-            if ( arr1[i1] < arr2[i2] )  {
+        while (i1 < N1 && i2 < N2) {
+            if (arr1[i1] < arr2[i2]) {
                 arr[i] = arr1[i1];
                 i++;
                 i1++;
-            }
-            else    {
+            } else {
                 arr[i] = arr2[i2];
                 i2++;
                 i++;
             }
         }
-        while ( i1 < N1) {
+        while (i1 < N1) {
             arr[i] = arr1[i1];
             i1++;
             i++;
         }
-        while ( i2 < N2) {
+        while (i2 < N2) {
             arr[i] = arr2[i2];
             i2++;
             i++;
